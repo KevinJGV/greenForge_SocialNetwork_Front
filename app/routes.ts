4a9f0a69b6177e4981aae62def10d86994c627e2
Kevin Gonzalez <kejogo.dev@outlook.com>
2025-02-13 08:23:42 -0500

    fix: redireccion automatica establecida a '/home' provisionalmente

--- app/routes.ts
+++ app/routes.ts
@@ -4,15 +4,17 @@ import {
 	layout,
 	route,
 } from "@react-router/dev/routes";
+import { redirect } from "react-router";
 
 export default [
+	redirect("/home"),
 	layout("layouts/initSession.tsx", [
 		route("/signin", "routes/signin.tsx"),
 		route("/signup", "routes/signup.tsx"),
 
 	]),
 
-	// layout("layouts/homeLayout.tsx", [
-	// 	route("/", "routes/home.tsx")
-	// ]),
+	layout("layouts/homeLayout.tsx", [
+		route("/", "routes/home.tsx"),
+	])
 ] as RouteConfig;
