1150bc9587998235bea0fa530d57347a6621a7fa
Kevin Gonzalez <kejogo.dev@outlook.com>
2025-02-13 07:56:38 -0500

    fix: corregir definición de tipo en notificationContext.tsx

--- app/components/notificationContext.tsx
+++ app/components/notificationContext.tsx
@@ -8,7 +8,8 @@ import {
 } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-type Props = {sadode;
+type Props = {
+	children: React.ReactNode;
 };
 
 type ToastPromiseParamsCustom = {
